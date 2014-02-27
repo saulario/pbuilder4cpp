dacf091409a0a330de262ea5acfb533001a0f4a5
saul.correas@gmail.com <saul.correas@gmail.com@70564e2e-3378-c854-0daf-fdcc2791133a>
2014-02-27 11:48:10 +0000

    Created in rev:65

---
+++ tags/r1402rc0/pbuilder/pbuilder.h
@@ -0,0 +1,132 @@
+/*
+ * Persistence Builder (pbuilder)
+ * Copyright (C) 2013..  Saul Correas Subias 
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+#ifndef PBUILDER_H
+#define	PBUILDER_H
+
+#include <iostream>
+#include <list>
+#include <log4cxx/logger.h>
+#include <map>
+
+namespace pbuilder {
+
+    enum MODEL_TYPE {
+        SMALLINT,
+        MEDIUMINT,
+        INTEGER,
+        BIGINT,
+        FLOAT,
+        DOUBLE,
+        STRING,
+        CLOB,
+        DATE,
+        TIME,
+        DATETIME,
+        TIMESTAMP,
+        BIT,
+        BLOB,
+        POINT,
+        LINE,
+        BOX,
+        POLYGON,
+        CIRCLE
+    };
+
+    struct Unit {
+    public:
+        std::string name;
+        std::string database;
+        std::string url;
+        std::string ns;
+        bool geometry;
+        std::string dateInitialization;
+        std::string datetimeInitialization;
+        std::string timeInitialization;
+
+        Unit() : name(""), database(""), url(""), ns(""), geometry(false)
+        , dateInitialization(""), datetimeInitialization(""), timeInitialization("") {
+        };
+
+    };
+
+    struct Column {
+    public:
+        std::string name;
+        int position;
+        bool isNullable;
+        std::string schemaType;
+        MODEL_TYPE type;
+        long charMaxLength;
+        long numericPrecision;
+        long numericScale;
+        bool isUnsigned;
+        std::string defaultValue;
+        std::string comment;
+
+        Column() : name(""), position(0), isNullable(false), schemaType("")
+        , type(STRING), charMaxLength(0L), numericPrecision(0L), numericScale(0L)
+        , isUnsigned(false), defaultValue(""), comment("") {
+        };
+    };
+
+    struct Table {
+    public:
+        std::string name;
+        std::list<Column> columns;
+        std::list<Column> pkColumns;
+
+        Table(const std::string & pname) : name(pname) {
+        }
+
+    };
+
+    struct Model {
+    public:
+        std::string name;
+        std::map<std::string, Table> tables;
+
+        Model() : name("") {
+        }
+    };
+
+    class PersistenceBuilder {
+    private:
+        static log4cxx::LoggerPtr logger;
+
+        std::string configFile;
+
+        void analyze(void);
+        void checkConfiguration(void);
+        void evaluateParameters(int argc, char ** argv);
+        void render(void);
+
+    public:
+        Unit unit;
+        Model model;
+        std::string table;
+
+        PersistenceBuilder() : table(""), configFile("") {
+        }
+        int main(int argc, char ** argv);
+
+    };
+
+}
+
+#endif	/* PBUILDER_H */
---
+++ tags/r1402rc0/pbuilder/pbuilder_render_TNTDBRender.cpp
@@ -0,0 +1,283 @@
+/*
+ * Persistence Builder (pbuilder)
+ * Copyright (C) 2013..  Saul Correas Subias 
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+#include <iomanip>
+
+#include "pbuilder.h"
+#include "pbuilder_render.h"
+
+using namespace pbuilder::render;
+
+log4cxx::LoggerPtr TNTDBRender::logger = log4cxx::Logger::getLogger("pbuilder::render::TNTDBRender");
+
+TNTDBRender::TNTDBRender(Render* parent_) : parent(parent_) {
+    dateInitialization = "tntdb::Date()";
+    if (!parent->pbuilder->unit.dateInitialization.empty()) {
+        dateInitialization = parent->pbuilder->unit.dateInitialization;
+    }
+    datetimeInitialization = "tntdb::Datetime()";
+    if (!parent->pbuilder->unit.datetimeInitialization.empty()) {
+        datetimeInitialization = parent->pbuilder->unit.datetimeInitialization;
+    }
+    timeInitialization = "tntdb::Time()";
+    if (!parent->pbuilder->unit.timeInitialization.empty()) {
+        timeInitialization = parent->pbuilder->unit.timeInitialization;
+    }    
+}
+
+std::string TNTDBRender::asText(const pbuilder::Column & column_) {
+    LOG4CXX_TRACE(logger, "asText -----> begin");
+    std::string value = "";
+    switch (column_.type) {
+        case SMALLINT:
+            value = std::string((column_.isUnsigned ? "unsigned " : "")) + "short";
+            break;
+        case MEDIUMINT:
+            value = std::string((column_.isUnsigned ? "unsigned " : "")) + "int";
+            break;
+        case INTEGER:
+            value = std::string((column_.isUnsigned ? "unsigned " : "")) + "int";
+            break;
+        case BIGINT:
+            value = std::string((column_.isUnsigned ? "unsigned " : "")) + "long";
+            break;
+        case FLOAT:
+            value = "float";
+            break;
+        case DOUBLE:
+            value = "double";
+            break;
+        case STRING:
+        case CLOB:
+            value = "std::string";
+            break;
+        case BLOB:
+            value = "tntdb::Blob";
+            break;
+        case DATE:
+            value = "tntdb::Date";
+            break;
+        case TIME:
+            value = "tntdb::Time";
+            break;
+        case DATETIME:
+        case TIMESTAMP:
+            value = "tntdb::Datetime";
+            break;
+        default:
+            value = "std::string";
+    }
+    LOG4CXX_TRACE(logger, "asText <----- end");
+    return value;
+}
+
+std::string TNTDBRender::defaultValue(const pbuilder::Column & column_) {
+    LOG4CXX_TRACE(logger, "defaultValue -----> begin");
+    std::string value = "";
+    switch (column_.type) {
+        case SMALLINT:
+            value = "0";
+            break;
+        case MEDIUMINT:
+            value = "0";
+            break;
+        case INTEGER:
+            value = "0";
+            break;
+        case BIGINT:
+            value = "0L";
+            break;
+        case FLOAT:
+            value = "0.0";
+            break;
+        case DOUBLE:
+            value = "0.0";
+            break;
+        case STRING:
+        case CLOB:
+            value = "\"\"";
+            break;
+        case BLOB:
+            value = "tntdb::Blob()";
+            break;
+        case DATE:
+            value = dateInitialization;
+            break;
+        case TIME:
+            value = timeInitialization;
+            break;
+        case DATETIME:
+        case TIMESTAMP:
+            value = datetimeInitialization;
+            break;
+        default:
+            value = "\"\"";
+    }
+    LOG4CXX_TRACE(logger, "defaultValue <----- end");
+    return value;
+}
+
+void TNTDBRender::notify(void) {
+    LOG4CXX_TRACE(logger, "doRender -----> begin");
+
+    TNTDBArtifactDeclarationRender adec(this);
+    TNTDBArtifactDefinitionRender adef(this);
+    TNTDBEntityDeclarationRender edec(this);
+    TNTDBEntityDefinitionRender edef(this);
+
+    adec.notify();
+    adef.notify();
+    edec.notify();
+    edef.notify();
+
+    LOG4CXX_TRACE(logger, "doRender <----- end");
+}
+
+std::string TNTDBRender::rowGet(const pbuilder::Column & column_) {
+    LOG4CXX_TRACE(logger, "rowGet -----> begin");
+    std::string value = "row.get";
+    switch (column_.type) {
+        case SMALLINT:
+            if (column_.isUnsigned) {
+                value += "Unsigned";
+            } else {
+                value += "Int";
+            }
+            break;
+        case MEDIUMINT:
+            if (column_.isUnsigned) {
+                value += "Unsigned";
+            } else {
+                value += "Int";
+            }
+            break;
+        case INTEGER:
+            if (column_.isUnsigned) {
+                value += "Unsigned";
+            } else {
+                value += "Int";
+            }
+            break;
+        case BIGINT:
+            if (column_.isUnsigned) {
+                value += "Unsigned64";
+            } else {
+                value += "Int64";
+            }
+            break;
+        case FLOAT:
+            value += "Float";
+            break;
+        case DOUBLE:
+            value += "Double";
+            break;
+        case STRING:
+        case CLOB:
+            value += "String";
+            break;
+        case BLOB:
+            value += "Blob";
+            break;
+        case DATE:
+            value += "Date";
+            break;
+        case TIME:
+            value += "Time";
+            break;
+        case DATETIME:
+        case TIMESTAMP:
+            value += "Datetime";
+            break;
+        default:
+            value = "String";
+    }
+    value += "(index++)";
+
+    LOG4CXX_TRACE(logger, "rowGet <----- end");
+    return value;
+}
+
+std::string TNTDBRender::stmtSet(const pbuilder::Column & column_, const bool & nullable_) {
+    LOG4CXX_TRACE(logger, "stmtSet -----> begin");
+    std::string value = "stmt.set";
+    std::string suffix = "";
+    switch (column_.type) {
+        case SMALLINT:
+            if (column_.isUnsigned) {
+                value += "Unsigned";
+            } else {
+                value += "Int";
+            }
+            break;
+        case MEDIUMINT:
+            if (column_.isUnsigned) {
+                value += "Unsigned";
+            } else {
+                value += "Int";
+            }
+            break;
+        case INTEGER:
+            if (column_.isUnsigned) {
+                value += "Unsigned";
+            } else {
+                value += "Int";
+            }
+            break;
+        case BIGINT:
+            if (column_.isUnsigned) {
+                value += "Unsigned64";
+            } else {
+                value += "Int64";
+            }
+            break;
+        case FLOAT:
+            value += "Float";
+            break;
+        case DOUBLE:
+            value += "Double";
+            break;
+        case STRING:
+        case CLOB:
+            value += "String";
+            break;
+        case BLOB:
+            value += "Blob";
+            break;
+        case DATE:
+            value += "Date";
+            break;
+        case TIME:
+            value += "Time";
+            break;
+        case DATETIME:
+        case TIMESTAMP:
+            value += "";
+            suffix = ".getIso()";
+            break;
+        default:
+            value = "String";
+    }
+    if (!nullable_) {
+        value += "(\"" + column_.name + "\", e->" + column_.name + suffix + ");";
+    } else {
+        value += "(\"" + column_.name + "\", e->get" + parent->toUpper(column_.name) + "());";
+    }
+
+    LOG4CXX_TRACE(logger, "stmtSet <----- end");
+    return value;
+}
